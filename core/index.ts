e1a79d4fe0982c08c36e8952600528f000606d30
justi <jlo2@csub.edu>
2026-04-24 21:30:00 -0700

    feat: initialize TypeScript project structure

---
+++ core/index.ts
@@ -0,0 +1,7 @@
+// Core exports will be added as we implement each module
+export * from './types';
+export * from './law-loader';
+export * from './pattern-matcher';
+export * from './evaluator';
+export * from './hook-registry';
+export * from './fix-validator';
